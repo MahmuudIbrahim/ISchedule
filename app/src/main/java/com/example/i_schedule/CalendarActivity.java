bafd41910ae6b47d3a00810911ce8346b9ef4c8f
MahmuudIbrahim <144620931+MahmuudIbrahim@users.noreply.github.com>
2023-11-15 20:50:20 -0500

    Create vcs.xml

--- app/src/main/java/com/example/i_schedule/CalendarActivity.java
+++ app/src/main/java/com/example/i_schedule/CalendarActivity.java
@@ -1,7 +1,14 @@
 package com.example.i_schedule;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.CalendarView;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -18,10 +25,57 @@ public class CalendarActivity extends AppCompatActivity {
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
-                // Handle the selected date here
                 String selectedDate = year + "-" + (month + 1) + "-" + day;
                 Toast.makeText(CalendarActivity.this, "Selected Date: " + selectedDate, Toast.LENGTH_SHORT).show();
+                showAddEventDialog();
             }
         });
     }
-}
+
+    private void showAddEventDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Add Event");
+
+        View view = LayoutInflater.from(this).inflate(R.layout.dialog_add_event, null);
+        builder.setView(view);
+
+        final EditText dateEditText = view.findViewById(R.id.editTextDate);
+        final EditText timeEditText = view.findViewById(R.id.editTextTime);
+        final EditText locationEditText = view.findViewById(R.id.editTextLocation);
+        final EditText descriptionEditText = view.findViewById(R.id.editTextDescription);
+
+        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                // Retrieve the values from the EditTexts
+                String date = dateEditText.getText().toString();
+                String time = timeEditText.getText().toString();
+                String location = locationEditText.getText().toString();
+                String description = descriptionEditText.getText().toString();
+
+                Intent intent = new Intent(Intent.ACTION_INSERT)
+                        .setData(CalendarContract.Events.CONTENT_URI)
+                        .putExtra(CalendarContract.Events.TITLE, description)
+                        .putExtra(CalendarContract.Events.EVENT_LOCATION, location)
+                        .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, eventTime)
+                        .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, eventTime + 60 * 60 * 1000) // 1 hour duration
+                        .putExtra(CalendarContract.Events.DESCRIPTION, description);
+
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(CalendarActivity.this, "No calendar app found on the device", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                // User canceled the event addition
+            }
+        });
+
+        builder.show();
+    }
+
